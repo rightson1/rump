13e1a460fb06b5f0d8d3337fd39a8d5b0a4469bb
rightson <chari.rightson@gmail.com>
2023-03-08 10:59:33 +0300

    update firebase.jsx

--- utils/firebase.jsx
+++ utils/firebase.jsx
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyCECoxYRp4k1V1PNlSiSp0XgkN5eB8wJ40",
-    authDomain: "weed-e4552.firebaseapp.com",
-    projectId: "weed-e4552",
-    storageBucket: "weed-e4552.appspot.com",
-    messagingSenderId: "915327604976",
-    appId: "1:915327604976:web:7afa6bb2e717d783e0ca35"
+    apiKey: "AIzaSyAzVl3Y0Bkp05ySMYTPfxbYXyhw-Bgc7Ik",
+    authDomain: "rump-f8ec2.firebaseapp.com",
+    projectId: "rump-f8ec2",
+    storageBucket: "rump-f8ec2.appspot.com",
+    messagingSenderId: "673393457957",
+    appId: "1:673393457957:web:dd23ff58478b9fda3838cc"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
